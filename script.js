08e750ed627f1e3e0029fe9cf92ca684565ffa69
Levendor <myth_of_lavanda@mail.ru>
2020-10-30 22:33:58 +0300

    feat: add shift, caps and lang joint work

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ const Keyboard = {
     capsLock: false,
     shift: false,
     fn: false,
-    language: true
+    language: true,
+		layoutCounter: 1
   },
 
   init() {
@@ -47,28 +48,28 @@ const Keyboard = {
       "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\",
       "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "\'", "enter",
       "shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "\/", "up",
-      "done", "EN", "mic", "Fn", "space", "win", "left", "down", "right"
+      "done", "EN", "mic", "space", "win", "left", "down", "right"
     ];
-    this.elements.layouts[2] = [
+    this.elements.layouts[3] = [
       "\`", "!", "\@", "\#", "\$", "\%", "\^", "\&", "\*", "\(", "\)", "\_", "\+", "backspace",
       "tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|",
       "caps", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "\"", "enter",
       "shift", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "\?", "up",
-      "done", "EN", "mic", "Fn", "space", "win", "left", "down", "right"
+      "done", "EN", "mic", "space", "win", "left", "down", "right"
     ];
-    this.elements.layouts[3] = [
+    this.elements.layouts[0] = [
       "ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace",
       "tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "\\",
       "caps", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "enter",
       "shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".", "up",
-      "done", "RU", "mic", "Fn", "space", "win", "left", "down", "right"
+      "done", "RU", "mic", "space", "win", "left", "down", "right"
     ];
-    this.elements.layouts[4] = [
+    this.elements.layouts[2] = [
       "Ё", "!", "\"", "№", ";", "%", ":", "?", "*", "(", ")", "_", "+", "backspace",
       "tab", "Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ", "\/",
       "caps", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", "enter",
       "shift", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", ",", "up",
-      "done", "RU", "mic", "Fn", "space", "win", "left", "down", "right"
+      "done", "RU", "mic", "space", "win", "left", "down", "right"
     ];
 
     const createIcon = (iconName) => {
@@ -145,6 +146,7 @@ const Keyboard = {
           break;
 
         case 'EN':
+          keyElement.classList.add('keyboard__key--wide');
           keyElement.innerHTML = '<span>EN</span>';
           keyElement.classList.add('language');
           keyElement.addEventListener('click', (e) => {
@@ -154,14 +156,8 @@ const Keyboard = {
           break;
 
         case 'mic':
+          keyElement.classList.add('keyboard__key--wide', 'keyboard__key--activatable');
           keyElement.innerHTML = createIcon('mic_off');
-          keyElement.addEventListener('click', () => {
-            
-          });
-          break;
-
-        case 'Fn':
-          keyElement.innerHTML = '<span>Fn</span>';
           keyElement.addEventListener('click', () => {
             
           });
@@ -177,6 +173,7 @@ const Keyboard = {
           break;
 
         case 'win':
+          keyElement.classList.add('keyboard__key--wide');
           keyElement.innerHTML = '<span class="iconify" data-icon="mdi-microsoft-windows"></span>';
           keyElement.addEventListener('click', () => {
             
@@ -208,7 +205,8 @@ const Keyboard = {
           keyElement.textContent = key.toLowerCase();
 
           keyElement.addEventListener("click", () => {
-            this.properties.value += this.properties.capsLock ^ this.properties.shift ? keyElement.textContent.toUpperCase() : keyElement.textContent.toLowerCase();
+            // this.properties.value += this.properties.capsLock ^ this.properties.shift ? keyElement.textContent.toUpperCase() : keyElement.textContent.toLowerCase();
+            this.properties.value += keyElement.textContent;
             this.triggerEvent("oninput");
           });
 
@@ -233,52 +231,59 @@ const Keyboard = {
 
   toggleCapsLock() {
     this.properties.capsLock = !this.properties.capsLock;
-    for (const key of this.elements.keys) {
-      if (key.childElementCount === 0) {
-        key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
-      }
-    }
+		this.updateKeyboard();
     caps.classList.toggle('keyboard__key--active', this.properties.capsLock);
   },
 
   toggleShift() {
-    let n;
     this.properties.shift = !this.properties.shift;
-    if (this.properties.shift) n = 2
-    else n = 1;
-    for (let i = 0; i < this.elements.keys.length; i++) {
-      if (this.elements.keys[i].childElementCount === 0) {        
-        switch(this.properties.language) {
-          case true:
-            this.elements.keys[i].textContent = this.elements.layouts[n][i];
-            break;
-          case false:
-            this.elements.keys[i].textContent = this.elements.layouts[2 + n][i];
-            break;
-        }
-      }      
-    }
-    // if (this.properties.capsLock) key.textContent = !this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+		this.properties.layoutCounter += this.properties.shift  ? 2 : -2;
+		this.updateKeyboard();
     shift.classList.toggle('keyboard__key--active', this.properties.shift);
   },
 
   toggleLanguage(e) {
     e.target.innerHTML = this.properties.language  ? '<span>EN</span>' : '<span>RU</span>';
-    if (this.properties.shift) this.toggleShift();
-    if (this.properties.capsLock) this.toggleCapsLock();
-    for (let i = 0; i < this.elements.keys.length; i++) {
-      if (this.elements.keys[i].childElementCount === 0) {        
-        switch(this.properties.language) {
-          case true:
-            this.elements.keys[i].textContent = this.elements.layouts[1][i];
-            break;
-          case false:
-            this.elements.keys[i].textContent = this.elements.layouts[3][i];
-            break;
-        }
-      }
-    }
+		this.properties.layoutCounter += this.properties.language  ? 1 : -1;
+		this.updateKeyboard();
   },
+	
+	updateKeyboard() {
+		console.log(this.properties.layoutCounter);
+		switch(this.properties.layoutCounter) {
+			case 1:
+				for (let i = 0; i < this.elements.keys.length; i++) {
+					if (this.elements.keys[i].childElementCount === 0) {
+						this.elements.keys[i].textContent = this.properties.capsLock ? this.elements.layouts[1][i].toUpperCase() : this.elements.layouts[1][i].toLowerCase();
+					}
+				}
+				break;
+
+			case 3:
+				for (let i = 0; i < this.elements.keys.length; i++) {
+					if (this.elements.keys[i].childElementCount === 0) {
+						this.elements.keys[i].textContent = !this.properties.capsLock ? this.elements.layouts[3][i].toUpperCase() : this.elements.layouts[3][i].toLowerCase();
+					}
+				}
+				break;
+
+			case 0:
+				for (let i = 0; i < this.elements.keys.length; i++) {
+					if (this.elements.keys[i].childElementCount === 0) {
+						this.elements.keys[i].textContent = this.properties.capsLock ? this.elements.layouts[0][i].toUpperCase() : this.elements.layouts[0][i].toLowerCase();
+					}
+				}
+				break;
+
+			case 2:
+				for (let i = 0; i < this.elements.keys.length; i++) {
+					if (this.elements.keys[i].childElementCount === 0) {
+						this.elements.keys[i].textContent = !this.properties.capsLock ? this.elements.layouts[2][i].toUpperCase() : this.elements.layouts[2][i].toLowerCase();
+					}
+				}
+				break;
+		}
+	},
 
   open(initialValue, oninput, onclose) {
     this.properties.value = initialValue || '';
